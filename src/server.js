a6341691f05c7665bf09421efebed77e192a6a6f
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-18 18:03:18 -0300

    model -> criando registros na tabela

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
@@ -7,8 +8,19 @@ const app = express();
 // Iniciando o BD
 mongoose.connect('mongodb://192.168.99.100:27017/mongodb_starter', { useNewUrlParser: true, useUnifiedTopology: true });
 
+requireDir('../src/models');
+
+const Product = mongoose.model('Product');
+
+// Rota GET
 app.get('/', (req, res) => {
-  res.send('Faaaala Viaaadxx');
+  Product.create({
+    title: 'Guitarra Fender Stratocaster',
+    description: 'guitarra americana de 1980',
+    url: 'https://br.pinterest.com/pin/822258844453825780/',
+  });
+
+  res.send('Produto criado');
 });
 
 app.listen(3333);
