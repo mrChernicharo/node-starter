8154043d5fcc880796b7f0132a29311c0494fd78
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-18 17:23:24 -0300

    mongo + mongoose

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o BD
+mongoose.connect('mongodb://192.168.99.100:27017/mongodb_starter', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
-  res.send('Faaaala Viaaad');
+  res.send('Faaaala Viaaadxx');
 });
 
 app.listen(3333);
